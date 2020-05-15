6897e5e3f09d0e8b683359bee8f2a6f20f3ac2e2
Shubham Kondekar <kondekarshubham123@gmail.com>
2020-05-16 04:04:35 +0530

    Moving snake

--- SnakeGame/snakegame.py
+++ SnakeGame/snakegame.py
@@ -25,8 +25,10 @@ game_over = False
 snake_x = 45
 snake_y = 55
 snake_size = 10
-
+fps = 30
 # Game Loop
+
+clock = pygame.time.Clock()
 
 while not exit_game:
 
@@ -35,10 +37,26 @@ while not exit_game:
 
         if event.type == pygame.QUIT:
             exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake_x+=10
+            
+            if event.key == pygame.K_LEFT:
+                snake_x-=10
+
+            if event.key == pygame.K_UP:
+                snake_y-=10
+
+            if event.key == pygame.K_DOWN:
+                snake_y+=10
+
+            
             
     gameWindow.fill(white)
     pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size,snake_size])
     pygame.display.update()
+    clock.tick(fps)
 
 pygame.quit()
 quit()
